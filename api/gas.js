166f761e35a79a806d195face3fa34773f71b157
gascheckking <warpai2k@gmail.com>
2025-05-12 00:08:10 +0200

    Delete api/gas.js
    Flyttar gas.js till rätt mapp

--- api/gas.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  try {
-    const response = await fetch('https://api.owlracle.info/v3/eth/gas?apikey=b9bee1f4421d4eebb170067dc3e2e579');
-    const data = await response.json();
-    res.status(200).json(data);
-  } catch (err) {
-    res.status(500).json({ error: 'Något gick fel med proxyn' });
-  }
-}
