facf7c3046d960451680ea45b33ed5185fdc70a1
gascheckking <warpai2k@gmail.com>
2025-05-11 23:51:15 +0200

    Update frame-action.js

--- api/api/frame-action.js
+++ api/api/frame-action.js
@@ -3,11 +3,14 @@ module.exports = (req, res) => {
   const { buttonIndex } = req.body.untrustedData;
   
   if (buttonIndex === 1) {
-    res.json({ type: 'redirect', url: 'https://gascheck-new.vercel.app' });
+    res.json({ 
+      type: 'redirect', 
+      url: 'https://gascheck-new.vercel.app' 
+    });
   } else if (buttonIndex === 2) {
     res.json({ 
       type: 'tx',
-      txUrl: 'https://gascheck-new.vercel.app/api/claim-tx'
+      txUrl: 'https://gascheck-new.vercel.app/api/claim-tx' 
     });
   }
 };
