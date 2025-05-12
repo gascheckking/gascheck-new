26f885829088ea65d040700dd627b02af1a35946
gascheckking <warpai2k@gmail.com>
2025-05-12 20:07:59 +0200

    Update frame-validator.js

--- api/frame-validator.js
+++ api/frame-validator.js
@@ -1,12 +1,10 @@
 import { NeynarAPIClient } from "@neynar/nodejs-sdk";
-
 const client = new NeynarAPIClient(process.env.NEYNAR_API_KEY);
 
 export async function validateFrameAction(req) {
   try {
     const body = await req.json();
     const { trustedData } = body;
-
     const result = await client.validateFrameAction(trustedData.messageBytes);
     return result.valid && result.action?.fid;
   } catch (err) {
