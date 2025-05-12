fe9e431861e7139a9bdeba61d9c8bae84426f026
gascheckking <warpai2k@gmail.com>
2025-05-12 20:08:52 +0200

    Update WarpXP.sol

--- contracts/WarpXP.sol
+++ contracts/WarpXP.sol
@@ -2,33 +2,19 @@
 pragma solidity ^0.8.0;
 
 contract WarpXP {
-    address public owner;
     mapping(address => uint256) public xp;
-    mapping(address => uint256) public lastClaim;
-
-    event XPClaimed(address indexed user, uint256 amount);
-
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Not authorized");
-        _;
-    }
-
-    constructor() {
-        owner = msg.sender;
-    }
-
+    mapping(address => bool) public premiumUsers;
+    
     function claimDaily() external {
-        require(block.timestamp > lastClaim[msg.sender] + 1 days, "Already claimed today");
         xp[msg.sender] += 100;
-        lastClaim[msg.sender] = block.timestamp;
-        emit XPClaimed(msg.sender, 100);
     }
 
-    function getXP(address user) external view returns (uint256) {
-        return xp[user];
+    function checkPremiumAccess() external view returns (bool) {
+        return premiumUsers[msg.sender];
     }
 
-    function adminSetXP(address user, uint256 amount) external onlyOwner {
-        xp[user] = amount;
+    function purchasePremium() external payable {
+        require(msg.value == 0.009 ether, "Incorrect ETH amount");
+        premiumUsers[msg.sender] = true;
     }
 }
