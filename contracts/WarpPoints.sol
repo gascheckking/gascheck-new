47026b4a2514e98f1446e7f9399389e90685c4b5
gascheckking <warpai2k@gmail.com>
2025-05-12 20:08:13 +0200

    Update WarpPoints.sol

--- contracts/WarpPoints.sol
+++ contracts/WarpPoints.sol
@@ -1,34 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract WarpPoints {
-    address public owner;
-    mapping(address => uint256) public balanceOf;
-
-    event PointsAwarded(address indexed user, uint256 amount);
-    event PointsSpent(address indexed user, uint256 amount);
-
-    constructor() {
-        owner = msg.sender;
+contract WarpXP {
+    mapping(address => uint256) public xp;
+    mapping(address => bool) public premiumUsers;
+    
+    function claimDaily() external {
+        xp[msg.sender] += 100;
     }
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Not authorized");
-        _;
+    function checkPremiumAccess() external view returns (bool) {
+        return premiumUsers[msg.sender];
     }
 
-    function awardPoints(address user, uint256 amount) external onlyOwner {
-        balanceOf[user] += amount;
-        emit PointsAwarded(user, amount);
-    }
-
-    function spendPoints(uint256 amount) external {
-        require(balanceOf[msg.sender] >= amount, "Not enough WP");
-        balanceOf[msg.sender] -= amount;
-        emit PointsSpent(msg.sender, amount);
-    }
-
-    function getBalance(address user) external view returns (uint256) {
-        return balanceOf[user];
+    function purchasePremium() external payable {
+        require(msg.value == 0.009 ether, "Incorrect ETH amount");
+        premiumUsers[msg.sender] = true;
     }
 }
